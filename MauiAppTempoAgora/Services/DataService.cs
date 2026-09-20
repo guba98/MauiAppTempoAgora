fe1eb003aaf558f4c87fbd9530afc9d5e8061863
guba98 <guba9779@gmail.com>
2026-09-20 12:19:18 -0300

    conteudo da aula 7

---
+++ MauiAppTempoAgora/Services/DataService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using MauiAppTempoAgora.Models;
+using Newtonsoft.Json.Linq;
+
+namespace MauiAppTempoAgora.Services
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisao(string cidade)
+        {
+            Tempo? t = null;
+
+            string chave = "a1ebfb658f51167ae709eec23534c6d2";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?" +
+                            $"q={cidade}&units=metric&appid={chave}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage resp = await client.GetAsync(url);
+
+                if(resp.IsSuccessStatusCode)
+                {
+                    string json = await resp.Content.ReadAsStringAsync();
+
+                    var rascunho = JObject.Parse(json);
+
+                    DateTime time = new();
+                    DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                    DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
+
+                    t = new()
+                    {
+                        lat = (double)rascunho["coord"]["lat"],
+                        lon = (double)rascunho["coord"]["lon"],
+                        description = (string)rascunho["weather"][0]["description"],
+                        main = (string)rascunho["weather"][0]["main"],
+                        temp_min = (double)rascunho["main"]["temp_min"],
+                        temp_max = (double)rascunho["main"]["temp_max"],
+                        sunrise = sunrise.ToString(),
+                        sunset = sunset.ToString(),
+                        speed = (double)rascunho["wind"]["speed"],
+                        visibility = (int)rascunho["visibility"]
+
+                    };
+                }
+            }
+
+
+
+            return t;
+        }
+    }
+}
